98f1fb0075f01f71923273cce1d26919ce07f0f2
Abimanyu Gatya Dhaneswara <manyuabim9@gmail.com>
2020-10-17 07:59:24 +0700

    socket.io simple app

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const socket = require("socket.io");
+
+// App Setup
+const PORT = 5000;
+const app = express();
+const server = app.listen(PORT, function () { 
+    console.log(`Listening on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+});
+
+// Static Files
+app.use(express.static("public"))
+
+// Socket setup
+const io = socket(server)
+
+io.on("connection", function (socket) {
+    console.log("Made socket connection");
+})
