fc530c5a0f78bb57d1f999c86571c64c0b19ca2b
Abimanyu Gatya Dhaneswara <manyuabim9@gmail.com>
2020-10-17 09:07:38 +0700

    socket.io simple app

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ app.use(express.static("public"))
 // Socket setup
 const io = socket(server)
 
+const activeUsers = new Set();
+
 io.on("connection", function (socket) {
     console.log("Made socket connection");
+
+    socket.on("new user", function (data) {
+        socket.userId = data
+        activeUsers.add(data)
+        io.emit("new user", [...activeUsers])
+    })
+
+    socket.on("disconnect", () => {
+        activeUsers.delete(socket.userId)
+        io.emit("user disconnected", socket.userId)
+    })
 })
